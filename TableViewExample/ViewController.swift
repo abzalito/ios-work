0ee94309b1b30c1863ee04e57992b84b2b746b38
Abzal Yeskali <abbzik@mail.ru>
2020-01-27 15:32:38 -0800

    Initial Commit

---
+++ TableViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewExample
+//
+//  Created by Yeskali Abzal on 1/27/20.
+//  Copyright © 2020 Yeskali Abzal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
